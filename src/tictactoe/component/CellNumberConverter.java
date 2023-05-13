55218a620a637fe9958deafbb87707572f24283f
Dzmitry Rusak <dzrusak@gmail.com>
2023-05-13 21:18:48 +0200

    Convert CellNumberConverter into interface

--- src/tictactoe/component/CellNumberConverter.java
+++ src/tictactoe/component/CellNumberConverter.java
@@ -21,27 +21,9 @@ package tictactoe.component;/*
 
 import tictactoe.model.Cell;
 
-public class CellNumberConverter {
-    private final char[][] mapping = {
-            {'7', '8', '9'},
-            {'4', '5', '6'},
-            {'1', '2', '3'}
-    };
+public interface CellNumberConverter {
+    Cell toCell(char number);
 
-    public Cell toCell(final char number) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (mapping[i][j] == number) {
-                    return new Cell(i, j);
-                }
-            }
-        }
-        return null;
-    }
-
-    public char toNumber(final Cell cell) {
-        return mapping[cell.getRow()][cell.getCol()];
-
-    }
+    char toNumber(Cell cell);
 
 }
